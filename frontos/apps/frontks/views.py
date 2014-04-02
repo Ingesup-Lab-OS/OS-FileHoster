9ca98fe0065c0347b80eb8c6f75480a8ac5b929a
adrien-louis-r <adrien.louis.r@gmail.com>
2014-04-02 10:12:09 +0200

    Woking file form

--- frontos/apps/frontks/views.py
+++ frontos/apps/frontks/views.py
@@ -2,15 +2,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from .forms import UploadFileForm
 from frontos.libs.utils import SwiftHelper
+from django.template import RequestContext
 
 swifthelper = SwiftHelper()
 
 def upload_file(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/')
+        for filename, file in request.FILES.iteritems():
+            print request.FILES[filename].name
+            print request.FILES[filename].size
+        return HttpResponseRedirect('/upload_file/')
     else:
         form = UploadFileForm()
-    return render_to_response('file/upload.html', {'form': form})
+        return render_to_response('file/upload.html',  {'form' : form }, context_instance=RequestContext(request))
