dff6246913f07a9bc0e889bf21bfe5a0c3490d3e
adrien-louis-r <adrien.louis.r@gmail.com>
2014-04-14 17:56:11 +0200

    Simple users features

--- os_file_hoster/apps/user_manager/urls.py
+++ os_file_hoster/apps/user_manager/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 from os_file_hoster.apps.user_manager import views
 
 urlpatterns = patterns('',
-    url(r'^user/new/$', views.user_new, name='user_new'),
-    url(r'^user/list/$', views.user_list, name='user_list'),
+    url(r'^new/$', views.user_new, name='user_new'),
+    url(r'^list/$', views.user_list, name='user_list'),
+    url(r'^delete/(?P<id>\w+)/$', views.user_delete, name='user_delete'),
 )
--- os_file_hoster/apps/user_manager/views.py
+++ os_file_hoster/apps/user_manager/views.py
@@ -1,14 +1,12 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from os_file_hoster.libs.utils import KeystoneHelper
-from django.template import RequestContext
+from os_file_hoster.utils.os_helper import KeystoneHelper
 
 kshelper = KeystoneHelper()
 
 def user_list(request):
-    ksadmin = kshelper.get_ksadmin()
-    users = ksadmin.users.list(tenant_id = "6ca0f39be8bc4b208abb4dbd1f9eb1e2")
+    users = kshelper.get_ksadmin(request).users.list(tenant_id = "6ca0f39be8bc4b208abb4dbd1f9eb1e2")
     user_list = []
     for user in users:
         # print user._info
@@ -20,19 +18,20 @@ def user_list(request):
         }
         user_list.append(user)
 
-    print user_list[0]
-    return render_to_response('user/list.html', {'users' : user_list}, context_instance=RequestContext(request))
+    return render(request, 'user/list.html', {'users' : user_list})
 
 def user_new(request):
-    print "here"
     if request.method == 'POST':
-        ksclient = kshelper.get_ksadmin_fom_token_id(request.user.token.id)
-        print ksclient
+        kshelper.get_ksadmin(request)
         user = request.POST.get('username', '')
         password = request.POST.get('password', '')
         email = request.POST.get('email', '')
 
-        kshelper.create_ksuser(ksclient, user, password, email)
-        return HttpResponseRedirect(reverse('UserManager:user_list'))
+        kshelper.create_ksuser(user, password, email)
+        return HttpResponseRedirect(reverse('user_list'))
     else:
-        return render_to_response('user/new.html', context_instance=RequestContext(request))
+        return render(request, 'user/new.html')
+
+def user_delete(request, id):
+    kshelper.delete_ksuser(id)
+    return HttpResponseRedirect('/');
